f0d211fc59aab30b56bbfc9e1cd902e83765accb
zio-git <73478973+zio-git@users.noreply.github.com>
2022-03-28 14:15:09 +0200

    1

--- art_shippingx.py
+++ art_shippingx.py
@@ -28,7 +28,7 @@ def alert(url, params):
     return r
 
 
-recipients = ["+265995246144", "+265998006237", "+265991450316", "+265999611280", "+265888741745", "+265999614130", "+265999537966"] 
+recipients = ["+265995246144", "+265998006237", "+265991450316", "+265998276712"] 
 
 cluster = get_xi_data('http://10.44.0.52/sites/api/v1/get_single_cluster/1')
 
@@ -58,7 +58,7 @@ for site_id in cluster['site']:
 
             version = msg.format(result).strip()
 
-            api_version = "v4.14.0"
+            api_version = "v4.14.3"
 
             if api_version == version:
                 msgx = "Hi there,\n\nDeployment of ART to " + version + " for " + site[
@@ -84,7 +84,7 @@ for site_id in cluster['site']:
             # make sure we are sending the alert at the last pint attempt
             if count == 3:
                 for recipient in recipients:
-                    msg = "Hi there,\n\nDeployment of ART to v4.14.0 for " + site['name'] + " failed to complete after several connection attempts.\n\nThanks!\nEGPAF HIS."
+                    msg = "Hi there,\n\nDeployment of ART to v4.14.3 for " + site['name'] + " failed to complete after several connection attempts.\n\nThanks!\nEGPAF HIS."
                     params = {
                         "api_key": os.getenv('API_KEY'),
                         "recipient": recipient,
